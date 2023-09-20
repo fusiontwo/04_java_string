62cfaa3cf15f4aa586f894afb49ec2ec0cd813da
fusiontwo <yr0955@naver.com>
2023-09-21 00:26:34 +0900

    Commit

--- 04_java_string/src/step4_01/string/StringEx13_풀이.java
+++ 04_java_string/src/step4_01/string/StringEx13_풀이.java
@@ -1,5 +1,7 @@
 package step4_01.string;
 
+import java.util.Arrays;
+
 // # 정렬 (사전순으로 정렬해 보시오.)
 
 public class StringEx13_풀이 {
@@ -8,6 +10,21 @@ public class StringEx13_풀이 {
 		
 		String[] names = {"홍길동", "김유신", "마동석", "자바킹", "서동요"};
 
+		for (int i = 0; i < names.length; i++) {
+			String minName = names[i];
+			int minIdx = i;
+			for (int j = i; j < names.length; j++) {
+				if (minName.compareTo(names[j]) > 0) {
+					minName = names[j];
+					minIdx = j;
+				}
+			}
+			String temp = names[i];
+			names[i] = names[minIdx];
+			names[minIdx] = temp;
+		}
+		
+		System.out.println(Arrays.toString(names));
 	}
 
 }
